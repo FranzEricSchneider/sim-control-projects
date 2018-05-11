b96e155d6d64f80671258165061fc0872e4c5feb
Eric Schneider <eric@ubuntu.localdomain>
2018-05-11 17:50:31 -0400

    Added noise to sensor reading. TODO: control noise distribution e.g. make gaussian ALSO put reactoin limits on the output (dont allow super rapid swings)

--- sim_tools/sim.py
+++ sim_tools/sim.py
@@ -4,6 +4,7 @@ from collections import deque, namedtuple
 import logging
 import math
 import matplotlib.pyplot as plt
+import numpy as np
 import sys
 
 from controller import PIDArduino
@@ -57,11 +58,15 @@ def simulate_system(args):
     while timestamp < (args.interval * 60):
         timestamp += args.sampletime
 
+        # Make noise from -1 to 1, then scale by maximum
+        sensor_noise = ((np.random.random() - 0.5) * 2) * args.sensor_noise_max
+        sensor_state = sim.delayed_states[0] + sensor_noise
+
         # Calculates controller reaction
         if args.supress_output:
             output = 0.0
         else:
-            output = sim.controller.calc(sim.delayed_states[0], args.setpoint)
+            output = sim.controller.calc(sensor_state, args.setpoint)
             output = max(output, args.out_min)
             output = min(output, args.out_max)
         # Calculates the effects of the controller output on the next sensor
@@ -145,53 +150,87 @@ def plot_simulation(simulation, title):
     plt.show()
 
 
-# Kettle
-# python sim_tools/sim.py --pid 104 0.8 205 --out-min -0.0 --out-max 100.0 --sampletime 5 --delay 15.0 --setpoint 45.0 --interval 20 --initial-values "{'kettle_temp': 40.0}" --constant-values "{'ambient_temp': 20.0, 'volume': 70.0, 'diameter': 50.0, 'heater_power': 6.0, 'heat_loss_factor': 1.0}" --plant Kettle
-# Pendulum:
-# python sim_tools/sim.py --pid 20 0.8 5 --out-min -10.0 --out-max 10.0 --sampletime 0.01 --delay 0.0 --setpoint 0.0 --interval 0.25 --initial-values "{'theta0': 0.3}" --constant-values "{'length': 0.5}" --plant InvertedPendulum
+'''
+Kettle
+python sim_tools/sim.py --pid 104 0.8 205 --out-min -0.0 --out-max 100.0 --sampletime 5 --delay 15.0 --setpoint 45.0 --interval 20 --initial-values "{'kettle_temp': 40.0}" --constant-values "{'ambient_temp': 20.0, 'volume': 70.0, 'diameter': 50.0, 'heater_power': 6.0, 'heat_loss_factor': 1.0}" --plant Kettle
+Pendulum:
+python sim_tools/sim.py --pid 20 0.8 5 --out-min -10.0 --out-max 10.0 --sampletime 0.01 --delay 0.0 --setpoint 0.0 --interval 0.2 --initial-values "{'theta0': 0.3, 'x0': 5}" --constant-values "{'length': 0.5}" --plant InvertedPendulum
+python sim_tools/sim.py --pid 20 0.8 5 --out-min -5.0 --out-max 5.0 --sampletime 0.01 --delay 0.05 --setpoint 0.0 --interval 0.2 --initial-values "{'theta_dot0': -1, 'x0': -5}" --constant-values "{'length': 0.5}" --plant InvertedPendulum --sensor-noise-max 0.01
+'''
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-P', '--plant', default='Kettle',
+        '-P', '--plant',
+        default='Kettle',
         help='the class from plant.py to simulate (e.g. InvertedPendulum)')
     parser.add_argument(
-        '-p', '--pid', nargs=3, metavar=('kp', 'ki', 'kd'),
-        default=None, help='simulate a PID controller')
+        '-p', '--pid',
+        nargs=3,
+        metavar=('kp', 'ki', 'kd'),
+        default=None,
+        help='simulate a PID controller')
 
     parser.add_argument(
-        '-v', '--verbose', action='store_true',
+        '-v', '--verbose',
+        action='store_true',
         help='be verbose')
 
     parser.add_argument(
-        '-s', '--setpoint', metavar='T', default=45.0, type=float,
-        help='target sensor value (default: 45)')
+        '-s', '--setpoint',
+        metavar='T',
+        default=45.0,
+        type=float,
+        help='target sensor value')
     parser.add_argument(
-        '-S', '--supress-output', action='store_true',
+        '-S', '--supress-output',
+        action='store_true',
         help='sets output to 0.0 so you can see system steady state')
 
     parser.add_argument(
-        '-i', '--interval', metavar='t', default=20, type=float,
-        help='simulated interval in minutes (default: 20)')
+        '-i', '--interval',
+        metavar='t',
+        default=20,
+        type=float,
+        help='simulated interval in minutes')
     parser.add_argument(
-        '-d', '--delay', metavar='t', default=15.0, type=float,
-        help='system response delay in seconds (default: 15)')
+        '-d', '--delay',
+        metavar='t',
+        default=15.0,
+        type=float,
+        help='system response delay in seconds')
     parser.add_argument(
-        '--sampletime', metavar='t', default=5.0, type=float,
-        help='sensor sample time in seconds (default: 5)')
+        '--sampletime',
+        metavar='t',
+        default=5.0,
+        type=float,
+        help='sensor sample time in seconds')
 
     parser.add_argument(
-        '--out-min', default=0.0,
-        type=float, help='minimum PID controller output (default: 0)')
+        '--out-min',
+        default=0.0,
+        type=float,
+        help='minimum PID controller output')
     parser.add_argument(
-        '--out-max', default=100.0,
-        type=float, help='maximum PID controller output (default: 100)')
+        '--out-max',
+        default=100.0,
+        type=float,
+        help='maximum PID controller output')
+    parser.add_argument(
+        '--sensor-noise-max',
+        default=0.0,
+        type=float,
+        help='Maximum noise that will be applied to the sensor readings')
 
     parser.add_argument(
-        '--constant-values', default='{}', action='store',
+        '--constant-values',
+        default='{}',
+        action='store',
         help='Pass in a dictionary of constants values used throughout the'
         ' simulation as a string, specific to each plant')
     parser.add_argument(
-        '--initial-values', default='{}', action='store',
+        '--initial-values',
+        default='{}',
+        action='store',
         help='Pass in a dictionary of initial values as a string, specific'
         ' to each plant')
 
